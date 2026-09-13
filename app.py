b946aa18b5c02a8eb751589f625820b02f0efe32
Sudhanshu Pandey <57751726+sudhanshuvlog@users.noreply.github.com>
2026-09-13 11:30:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def create_app():
     @app.route('/')
     def home():
         print("inside home function")
-        return 'hello hi'
+        return 'Hi GFG Workshop'
 
     @app.route('/test')
     def test():
